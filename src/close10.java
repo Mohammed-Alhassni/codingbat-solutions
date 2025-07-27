74cb1067fc66ee3797d34880b50196d98430a42f
mohammed <mohammed@domain.com>
2025-07-27 08:39:58 +0400

    Solution for  Warmup-1/close10

---
+++ src/close10.java
@@ -0,0 +1,16 @@
+package src;
+
+public class close10 {
+    public int close10(int a, int b) {
+        int first = Math.abs(10 - a);
+        int second = Math.abs(10 - b);
+
+        if (first < second){
+            return a;
+        } else if (first > second) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
+}
