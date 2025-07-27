9ffe2d43ef50c4d8ef1f725ac1cb04073d2aba7e
mohammed <mohammed@domain.com>
2025-07-27 09:10:30 +0400

    Solution for  Warmup-1/stringE

---
+++ src/stringE.java
@@ -0,0 +1,18 @@
+package src;
+
+public class stringE {
+    public boolean stringE(String str) {
+        int eCount=0;
+        for (char eChar: str.toCharArray()){
+            if (eChar == 'e'){
+                eCount += 1;
+            }
+        }
+
+        if (eCount >= 1 && eCount <= 3){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
