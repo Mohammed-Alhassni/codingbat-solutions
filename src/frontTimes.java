6359eb5bfb882775b0747b7f8e054ec31012ff6f
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-07-27 10:11:29 +0400

    Solution for  Warmup-1/frontTimes

---
+++ src/frontTimes.java
@@ -0,0 +1,15 @@
+package src;
+
+public class frontTimes {
+    public String frontTimes(String str, int n) {
+        String newStr= "";
+        for (int i= 1; i <= n; i++){
+            if (str.length() <= 3){
+                newStr += str;
+            } else {
+                newStr += str.substring(0, 3);
+            }
+        }
+        return newStr;
+    }
+}
