0a4285afbb4e86a336579215c7e751b2ffc4a2e6
mohammed <mohammed@domain.com>
2025-07-28 13:03:25 +0400

    Modified String-1/twoChar

--- src/String1/twoChar.java
+++ src/String1/twoChar.java
@@ -2,7 +2,7 @@ package src.String1;
 
 public class twoChar {
     public String twoChar(String str, int index) {
-        if (str.length()>2 && index<str.length()-1 && index >0){
+        if (index<str.length()-1 && index >0){
             return str.substring(index, index+2);
         }
         return str.substring(0,2);
