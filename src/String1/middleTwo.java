f65f701d0bcc21b90055090e8b64f45c739e833d
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-07-28 12:44:31 +0400

    Solution for  String-1/middleTwo

---
+++ src/String1/middleTwo.java
@@ -0,0 +1,8 @@
+package src.String1;
+
+public class middleTwo {
+    public String middleTwo(String str) {
+        int midIndex= str.length()/2;
+        return str.substring(midIndex-1,midIndex+1);
+    }
+}
