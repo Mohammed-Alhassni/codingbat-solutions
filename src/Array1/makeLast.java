e6b4f9411dce43a83b9180f2bbfad03eb39a8515
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-07-29 12:43:41 +0400

    Solution for  Array-1/makeLast

---
+++ src/Array1/makeLast.java
@@ -0,0 +1,15 @@
+package src.Array1;
+
+public class makeLast {
+    public int[] makeLast(int[] nums) {
+        int[] newArr= new int[nums.length * 2];
+        for (int i=0; i <newArr.length; i++){
+            if (i==newArr.length-1){
+                newArr[i]= nums[nums.length-1];
+            }else {
+                newArr[i]= 0;
+            }
+        }
+        return newArr;
+    }
+}
