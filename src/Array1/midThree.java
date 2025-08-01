6d40e7f21396a0b3768254422d022075f5bdbe73
mohammed <mohammed@domain.com>
2025-08-01 14:21:01 +0400

    Improved Array-1/midThree

--- src/Array1/midThree.java
+++ src/Array1/midThree.java
@@ -2,7 +2,7 @@ package src.Array1;
 
 public class midThree {
     public int[] midThree(int[] nums) {
-        int middleIndex=Math.round(nums.length/2);
+        int middleIndex=(nums.length-1)/2;
         int[] newArr = {nums[middleIndex-1], nums[middleIndex], nums[middleIndex+1]};
         return newArr;
     }
